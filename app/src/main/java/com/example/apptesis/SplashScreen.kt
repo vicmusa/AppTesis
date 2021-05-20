4042d6307965ac8c562b1b4a7bfbd16b0ab9e297
carlantortiz <carlantortiz@hotmail.com>
2021-05-20 16:16:55 -0400

    hola
    hola

---
+++ app/src/main/java/com/example/apptesis/SplashScreen.kt
@@ -0,0 +1,19 @@
+package com.example.apptesis
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreen : AppCompatActivity() {
+    lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        handler.postDelayed({
+            val intent=Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+}
