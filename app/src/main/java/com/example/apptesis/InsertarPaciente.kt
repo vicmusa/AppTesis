7e18af38f3ee4ba990dd2859b35119b075f31f1c
carlantortiz <carlantortiz@hotmail.com>
2021-06-08 18:30:36 -0400

    hh

---
+++ app/src/main/java/com/example/apptesis/InsertarPaciente.kt
@@ -0,0 +1,11 @@
+package com.example.apptesis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InsertarPaciente : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_insertar_paciente)
+    }
+}
