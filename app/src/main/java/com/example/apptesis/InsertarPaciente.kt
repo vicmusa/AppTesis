fd2ae94a6a0e2fb49206f189e7d015ddcb361fd9
vicmusa <59419072+vicmusa@users.noreply.github.com>
2021-06-20 12:23:37 -0400

    Updating app

--- app/src/main/java/com/example/apptesis/InsertarPaciente.kt
+++ app/src/main/java/com/example/apptesis/InsertarPaciente.kt
@@ -16,24 +16,25 @@ class InsertarPaciente : AppCompatActivity() {
     var fraccion: String = "Eta"
     var instrumento: String = "Lira"
     private lateinit var database: FirebaseFirestore
-    private lateinit var  txtName: EditText
-    private lateinit var  txtLastName: EditText
-    private lateinit var  txtEdad: EditText
-    private lateinit var  txtCI : EditText
-    private lateinit var  txtEstatura: EditText
-    private lateinit var  txtPeso: EditText
-    private lateinit var  txtID: EditText
-    private lateinit var grupoSanguineo : Spinner
+    private lateinit var txtName: EditText
+    private lateinit var txtLastName: EditText
+    private lateinit var txtEdad: EditText
+    private lateinit var txtCI: EditText
+    private lateinit var txtEstatura: EditText
+    private lateinit var txtPeso: EditText
+    private lateinit var txtID: EditText
+    private lateinit var grupoSanguineo: Spinner
     var name by Delegates.notNull<String>()
     var apellido by Delegates.notNull<String>()
     var edad by Delegates.notNull<String>()
     var CI by Delegates.notNull<String>()
-    var estatura  by Delegates.notNull<String>()
+    var estatura by Delegates.notNull<String>()
     var peso by Delegates.notNull<String>()
     var IDdisp by Delegates.notNull<String>()
     var sange by Delegates.notNull<String>()
-    var prevPato by Delegates.notNull<String>()
-    var alergias by Delegates.notNull<String>()
+    var prevPato = ""
+    var alergias = ""
+    var tiempo = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,48 +51,73 @@ class InsertarPaciente : AppCompatActivity() {
     private fun inicializar() {
         TODO("Not yet implemented, TERMINAR LA IMPLEMENTACION")
         database = FirebaseFirestore.getInstance()
-        txtName=findViewById(R.id.etxtnombre)
-        txtLastName=findViewById(R.id.etxtapellido)
-        txtEdad=findViewById(R.id.etxtedad)
-        txtCI=findViewById(R.id.etxtci)
-        txtEstatura=findViewById(R.id.etxtestatura)
-        txtPeso=findViewById(R.id.etxtpeso)
-        txtID=findViewById(R.id.etxtID)
-
+        txtName = findViewById(R.id.etxtnombre)
+        txtLastName = findViewById(R.id.etxtapellido)
+        txtEdad = findViewById(R.id.etxtedad)
+        txtCI = findViewById(R.id.etxtci)
+        txtEstatura = findViewById(R.id.etxtestatura)
+        txtPeso = findViewById(R.id.etxtpeso)
+        txtID = findViewById(R.id.etxtID)
+        grupoSanguineo = findViewById(R.id.gruprosang)
     }
 
-    fun cancelar(view: View) {onBackPressed()}
+    fun cancelar(view: View) {
+        onBackPressed()
+    }
+
     fun addPaciente(view: View) {
 
         if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(apellido)
-                && !TextUtils.isEmpty(edad) && !TextUtils.isEmpty(peso) && !TextUtils.isEmpty(estatura) && !TextUtils.isEmpty(CI)){
-            if(validate())
-            {
+            && !TextUtils.isEmpty(edad) && !TextUtils.isEmpty(peso) && !TextUtils.isEmpty(estatura) && !TextUtils.isEmpty(
+                CI
+            )
+        ) {
+            if (validate()) {
+                database.collection("pacientes").document(CI).set(
+                    hashMapOf(
+                        "nombre" to name,
+                        "apellido" to apellido,
+                        "edad" to edad,
+                        "peso" to peso,
+                        "estatura" to estatura,
+                        "Tipo de Sangre" to grupoSanguineo,
+                        "Patologías Previas" to prevPato,
+                        "Alergias" to alergias,
+                        "IDenUso" to IDdisp,
 
+
+                        )
+                ).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        Toast.makeText(applicationContext, R.string.addPacient, Toast.LENGTH_LONG)
+                            .show()
+                        onBackPressed()
+                    }
+                }
             }
-        }else
-        {
-            Toast.makeText(applicationContext,R.string.emptyfields, Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(applicationContext, R.string.emptyfields, Toast.LENGTH_LONG).show()
         }
     }
 
-    private fun validate() : Boolean {
+    private fun validate(): Boolean {
         TODO("Not yet implemented")
     }
 
-    fun setTipoSangre(view: View) {}
     fun usandoID(view: View) {
         if (view is CheckBox) {
             val checked: Boolean = view.isChecked
             when (view.id) {
                 R.id.usando -> {
                     if (checked) {
-                       txtID.visibility = View.VISIBLE
-                    } else {txtID.visibility = View.INVISIBLE
+                        txtID.visibility = View.VISIBLE
+                    } else {
+                        txtID.visibility = View.INVISIBLE
                     }
                 }
+            }
         }
+
+
     }
-
-
 }
