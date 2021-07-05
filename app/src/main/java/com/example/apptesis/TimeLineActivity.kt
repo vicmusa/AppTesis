d7e8a770c43cf77ab7e06e22accf51a4c8ead872
vicmusa <59419072+vicmusa@users.noreply.github.com>
2021-07-04 20:34:44 -0400

    Feat Timeline
    Se agrego graficas, se cambio los colores de las notas, todabvia existe un bug que al hacer zoom y cambiar la grafica de la persona crashea

--- app/src/main/java/com/example/apptesis/TimeLineActivity.kt
+++ app/src/main/java/com/example/apptesis/TimeLineActivity.kt
@@ -1,5 +1,6 @@
 package com.example.apptesis
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextWatcher
@@ -28,77 +29,79 @@ import com.google.firebase.ktx.Firebase
 import com.google.type.DateTime
 import java.sql.Date
 import java.sql.Timestamp
+import java.text.SimpleDateFormat
 
 class TimeLineActivity : AppCompatActivity() {
-    private  var list = mutableListOf<Historical_data>()
-    private  var listDato = mutableListOf<Paciente>()
-    private  lateinit var  db : FirebaseFirestore
-    private  lateinit var spinner : Spinner
-    private  lateinit var spinner2 : Spinner
-    private  lateinit var textV1: TextView
+    private var list = mutableListOf<Historical_data>()
+    private var listDato = mutableListOf<Paciente>()
+    private lateinit var db: FirebaseFirestore
+    private lateinit var spinner: Spinner
+    private lateinit var spinner2: Spinner
+    private lateinit var textV1: TextView
+    private val spo2Data: ArrayList<Entry> = arrayListOf()
+    private val hrData: ArrayList<Entry> = arrayListOf()
+    private val tempData: ArrayList<Entry> = arrayListOf()
+    private var ts = arrayListOf<Long>()
+    val dataSet: ArrayList<ILineDataSet> = arrayListOf()
     val dtbase = Firebase.database
-    private lateinit var lineChart : LineChart
-    private  lateinit var ref : DatabaseReference
+    private lateinit var lineChart: LineChart
+    private lateinit var ref: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_time_line)
-        spinner=findViewById(R.id.spinner)
-        spinner2=findViewById(R.id.spinner2)
-        val textV : TextView = findViewById(R.id.datos)
-        textV1  = findViewById(R.id.textView)
+        spinner = findViewById(R.id.spinner)
+        spinner2 = findViewById(R.id.spinner2)
+        val textV: TextView = findViewById(R.id.datos)
+        textV1 = findViewById(R.id.textView)
         lineChart = findViewById(R.id.grafico)
 
-        title="Time Line"
-        db= FirebaseFirestore.getInstance()
-        ref= dtbase.getReference("Historicos")
+        title = "Time Line"
+        db = FirebaseFirestore.getInstance()
+        ref = dtbase.getReference("Historicos")
 
 
         var datos = ""
 
-         db.collection("pacientes")
-             .whereNotEqualTo("idenUso","ninguna")
-             .get()
-             .addOnSuccessListener { resultado ->
-            for(documento in resultado)
-            {
-                // Obtengo todos los documentos que tienen ID y los inserto en una lista
-                val ci = documento.id
-                val name = documento["nombre"].toString()
-                val ap = documento["apellido"].toString()
-                val id = documento["idenUso"].toString()
-                val paciente = Paciente(name,ci,ap,id)
-                listDato.add(paciente)
-                datos +="${documento.id}: ${documento["nombre"]}\n"
+        db.collection("pacientes")
+                .whereNotEqualTo("idenUso", "ninguna")
+                .get()
+                .addOnSuccessListener { resultado ->
+                    for (documento in resultado) {
+                        // Obtengo todos los documentos que tienen ID y los inserto en una lista
+                        val ci = documento.id
+                        val name = documento["nombre"].toString()
+                        val ap = documento["apellido"].toString()
+                        val id = documento["idenUso"].toString()
+                        val paciente = Paciente(name, ci, ap, id)
+                        listDato.add(paciente)
+                        datos += "${documento.id}: ${documento["nombre"]}\n"
 
-            }
-                 val arraAdapter : ArrayAdapter<Paciente> = ArrayAdapter(this,android.R.layout.simple_spinner_item,listDato)
-                 textV.text = datos
-                 spinner.adapter= arraAdapter
+                    }
+                    val arraAdapter: ArrayAdapter<Paciente> = ArrayAdapter(this, android.R.layout.simple_spinner_item, listDato)
+                    textV.text = datos
+                    spinner.adapter = arraAdapter
 
 
-             }
+                }
     }
 
     private fun buscardatos(id: String) {
 
 
-            val listener = object :  ValueEventListener {
+        val listener = object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                if(snapshot.exists())
-                {
+                if (snapshot.exists()) {
                     list.clear()
-                    for(ds in snapshot.children)
-                    {
+                    for (ds in snapshot.children) {
 
                         val ts = ds.key.toString().toLong()
-                        val spo2= ds.child("spo2").value.toString().toFloat()
-                        val hr :Float= ds.child("hr").value.toString().toFloat()
-                        val temp= ds.child("temp").value.toString().toFloat()
-                        list.add(Historical_data(ts,spo2 ,hr,temp))
+                        val spo2 = ds.child("spo2").value.toString().toFloat()
+                        val hr: Float = ds.child("hr").value.toString().toFloat()
+                        val temp = ds.child("temp").value.toString().toFloat()
+                        list.add(Historical_data(ts, spo2, hr, temp))
                     }
-                }else
-                {
-                    Toast.makeText(this@TimeLineActivity, "EL ID NO EXISTE",Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this@TimeLineActivity, "EL ID NO EXISTE", Toast.LENGTH_LONG).show()
                 }
             }
 
@@ -117,7 +120,7 @@ class TimeLineActivity : AppCompatActivity() {
         val spId = spinner.selectedItemPosition
         textV1.text = listDato[spId].id
         buscardatos(listDato[spId].id)
-        val arrayAdapter: ArrayAdapter<Historical_data> = ArrayAdapter(this@TimeLineActivity,android.R.layout.simple_dropdown_item_1line,list)
+        val arrayAdapter: ArrayAdapter<Historical_data> = ArrayAdapter(this@TimeLineActivity, android.R.layout.simple_dropdown_item_1line, list)
         spinner2.adapter = arrayAdapter
         graficar()
 
@@ -125,46 +128,52 @@ class TimeLineActivity : AppCompatActivity() {
 
     private fun graficar() {
 
-        val spo2Data : ArrayList<Entry> = arrayListOf()
-        val hrData: ArrayList<Entry> = arrayListOf()
-        val tempData: ArrayList<Entry> = arrayListOf()
+        spo2Data.clear()
+        hrData.clear()
+        tempData.clear()
+        ts.clear()
 
-        for((i,it) in list.withIndex())
-        {
-            spo2Data.add(Entry(i.toFloat(),it.spo2))
-            hrData.add(Entry(i.toFloat(),it.hr))
-            tempData.add(Entry(i.toFloat(),it.temp))
+        for ((i, it) in list.withIndex()) {
+            spo2Data.add(Entry(i.toFloat(), it.spo2))
+            hrData.add(Entry(i.toFloat(), it.hr))
+            tempData.add(Entry(i.toFloat(), it.temp))
+            ts.add(it.ts)
+            Log.e("Valores","${it.ts}")
         }
 
-        val lineSpo2 =LineDataSet(spo2Data,"SPO2")
-        val lineHr =LineDataSet(hrData,"HR")
-        val lineTemp =LineDataSet(tempData,"Temp")
-        lineSpo2.color= R.color.teal_700
-        lineHr.color= R.color.red
-        lineSpo2.color= R.color.blue
-        val dataSet : ArrayList<ILineDataSet> = arrayListOf()
+        var lineSpo2 = LineDataSet(spo2Data, "SPO2")
+        var lineHr = LineDataSet(hrData, "HR")
+        var lineTemp = LineDataSet(tempData, "Temp")
+        lineSpo2.color = R.color.teal_700
+        lineHr.color = R.color.red
+        lineSpo2.color = R.color.blue
 
+        Log.e("Valores","Estoy AQUI")
+        dataSet.clear()
         dataSet.add(lineSpo2)
         dataSet.add(lineHr)
         dataSet.add(lineTemp)
-        val valorX :XAxis = lineChart.xAxis
-        valorX.labelCount=4
-        valorX.granularity=1F
-        valorX.valueFormatter = MyValue(list)
+        Log.e("Valores","Ahora AQUI")
+        var valorX: XAxis = lineChart.xAxis
+        valorX.labelCount = 0
+        valorX.position=XAxis.XAxisPosition.BOTTOM
+       // valorX.granularity = 1F
+
+        valorX.valueFormatter = object : ValueFormatter(){
+            override fun getFormattedValue(value: Float): String {
+                val fdate= SimpleDateFormat("dd/MM/yy@HH:mm:ss")
+                var date = fdate.format(Date((ts[value.toInt()]*1000)))
+                return date.toString()
+            }
+        }
 
         val data: LineData = LineData(dataSet)
-        lineChart.data=data
+        lineChart.data = data
         lineChart.invalidate()
 
 
     }
 
-private class MyValue (var lista : MutableList<Historical_data>) : ValueFormatter()  {
-    override fun getAxisLabel(value: Float, axis: AxisBase?): String {
-        val fdate=java.text.SimpleDateFormat("dd/MM/yyyy-HH:mm:ss")
-       val date = fdate.format(Date((lista[value.toInt()].ts*1000)))
-        return date
-    }
 }
-}
+
 
