b0a922e5fe6031a2371b4631d9173d5737574f25
vicmusa <59419072+vicmusa@users.noreply.github.com>
2021-09-27 20:13:23 -0400

    Update Repository.kt

--- app/src/main/java/com/example/apptesis/model/Repository.kt
+++ app/src/main/java/com/example/apptesis/model/Repository.kt
@@ -5,6 +5,10 @@ import com.example.apptesis.R
 class Repository {
 
     companion object{
+
+
+        // SINTOMAS
+
         val fiebre = InnerInfoModel("Fiebre", R.raw.fiebre,"FIEBRE...")
         val tos = InnerInfoModel("Tos Seca", R.raw.tos,"TOS SECA")
         val respirar = InnerInfoModel("Dificultad para respirar", R.raw.respirar,"DIFICULTAD PARA RESPIRAR")
@@ -15,9 +19,9 @@ class Repository {
         val perdida = InnerInfoModel("Perdida del gusto y el olfalto", R.raw.olfato,"PERDIDA DEL GUSTO Y EL OLFATO")
         //val conjuntivitis = InnerInfoModel("Conjuntivitis", R.drawable.conjuntivitis,"CONJUNTIVITIS")
         val diarrea = InnerInfoModel("Diarrea", R.raw.diarrea,"LA DIERRA ES UNA CAGALADERA DURISIMA")
-
-
         val listsintom = listOf(fiebre, tos, respirar, perdida, neumonia, garganta, cabeza, fatiga, diarrea)
+
+        // RECOMENDACIONES
 
         val distancia = InnerInfoModel("Distanciamiento social", R.raw.distancia,"Mantenga una distancia segura de los demás, incluso si no parecen estar enfermos. Se recomienda mantener 2 metros de distancia")
         val mascarilla = InnerInfoModel("Usar mascarilla", R.raw.mascarilla,"Use una mascarilla en público, especialmente en interiores o cuando no sea posible el distanciamiento físico.")
@@ -28,6 +32,14 @@ class Repository {
         val vacuna = InnerInfoModel("Vacúnese", R.raw.vacuna,"Vacúnese cuando sea su turno. Siga las pautas locales sobre vacunación.")
 
         val listrecom2 = listOf<InnerInfoModel>(distancia, mascarilla, espacios, manos, cara, casa, vacuna)
+
+
+        //INFOAPP
+
+        /*val oxi = InnerInfoModel("Oxigenación",R.raw.oxi,"Mediante un sensor el dispositivo es capaz de medir la oxigenación de la persona")
+        val hr = InnerInfoModel("Ritmo Cardiaco",R.raw.hr,"Mediante el mismo sensor es capaz de medir el ritmo cardiaco de la persona")
+        val temp = InnerInfoModel("Temperatura",R.raw.tempe,"Mediante un sensor el dispositivo es capaz de medir la oxigenación de la persona")
+        val listinfo = listOf(oxi,hr,temp)*/
 
         val recomendaciones = InfoModel("Recomendaciones", listrecom2)
         val sintomas = InfoModel("Sintomas", listsintom)
